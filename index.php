2a749978646fbbe145ae016aee0bb4411bc3c437
shcherbatsevichn <shcherbatsevich.n.dvl@yandex.by>
2022-01-11 09:52:09 +0300

    2-20

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php error_reporting(-1);
+//В массиве А(N) выбрать все элементы, встречающиеся только один раз.   
+$A = [1, -7, -4, 2, -7, 2, 3, 3, -199, 4, 1, 4, -20, 6, 6, 3, 8, -4, 13, 99];
+$p = 4;
+
+echo("A(n):<br>");
+print_r($A);
+echo("<br>Result:<br>");
+$a = get_uni_elem($A);
+print_r($a);
+
+function get_uni_elem($array){ //формируетт массив из уникальных эементов 
+    $res = [];
+    $n = 0;
+    for ($i = 0; $i < count($array); $i++){
+        if(count_elem_array($array, $array[$i]) == 1){
+           $res[$n] = $array[$i];
+           $n++; 
+        }
+    }
+    return $res;
+}        
+
+function count_elem_array($array, $elem){ //считает, сколько раз элемент встречается в массиве
+    $counter = 0;
+    for($i = 0; $i < count($array); $i++){
+        if($array[$i] == $elem){
+            $counter++;
+        }
+    }
+    return $counter;  
+}
